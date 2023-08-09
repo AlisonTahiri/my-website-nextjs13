08ccb2d5c64b3fcc1b9f2bdd9a8769217585de23
Alison Tahiri <alison.tahiri@gmail.com>
2023-08-09 13:47:14 +0200

    add intro and about sections

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import BgDecorator from "@/components/BgDecorator";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Header from "@/components/header";
+import Header from "@/components/sections/header";
 import { Providers } from "./providers";
 
 const inter = Inter({ subsets: ["latin"] });
