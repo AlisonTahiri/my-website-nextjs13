08df513fbe050f5cc660d218b65848f73c0ba0e8
Alison Tahiri <alison.tahiri@gmail.com>
2023-08-09 13:52:16 +0200

    add skills section

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import SectionDivider from "@/components/section-divider";
 import About from "@/components/sections/about";
 import Intro from "@/components/sections/intro";
 import Projects from "@/components/sections/projects";
+import Skills from "@/components/sections/skills";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <SectionDivider />
       <About />
       <Projects />
+      <Skills />
     </main>
   );
 }
