aa34166e0aa9e979859af6e84035c74e9f97fcdf
Alison Tahiri <alison.tahiri@gmail.com>
2023-08-11 11:49:01 +0200

    add google analytics feature.

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { Providers } from "./providers";
 import { Toaster } from "react-hot-toast";
 import Footer from "@/components/sections/footer";
 import ThemeSwitch from "@/components/theme-switcher";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,8 +22,21 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const GTM_ID = process.env.NEXT_PUBLIC_GTM_ID;
+
   return (
     <html suppressHydrationWarning lang="en" className="!scroll-smooth">
+      {/* Google Tag Manager Snippet */}
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+        `}
+      </Script>
+
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
@@ -34,6 +48,13 @@ export default function RootLayout({
           <ThemeSwitch />
         </Providers>
         <Toaster position="top-right" />
+
+        {/* Google Tag Manager No JS case */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+          }}
+        />
       </body>
     </html>
   );
